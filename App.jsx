8c7f7e4d75a22493e74c3c25caffc84dec405bfc
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-07-24 21:47:06 +0200

    Notes: add display by updatedat time

--- App.jsx
+++ App.jsx
@@ -5,12 +5,30 @@ import Split from "react-split"
 import { nanoid } from "nanoid"
 
 export default function App() {
+
+        /**
+     * Challenge:
+     * 1. ✅ Add createdAt and updatedAt properties to the notes
+     *    When a note is first created, set the `createdAt` and `updatedAt`
+     *    properties to `Date.now()`. Whenever a note is modified, set the
+     *    `updatedAt` property to `Date.now()`.
+     * 
+     * 2. Create a new `sortedNotes` array (doesn't need to be saved 
+     *    in state) that orders the items in the array from 
+     *    most-recently-updated to least-recently-updated.
+     *    This may require a quick Google search.
+     */
+
     const [notes, setNotes] = React.useState(
         () => JSON.parse(localStorage.getItem("notes")) || []
     )
     const [currentNoteId, setCurrentNoteId] = React.useState(
         (notes[0]?.id) || ""
     )
+
+    const sortedNotes = notes.sort((a, b) => b.updatedAt - a.updatedAt)
+    console.log(sortedNotes)
+
     
     const currentNote = 
         notes.find(note => note.id === currentNoteId) 
--- components/Editor.jsx
+++ components/Editor.jsx
@@ -1,6 +1,6 @@
 import React from "react"
-import R from "react-mde"
-const ReactMde = R.default
+import ReactMde from "react-mde"
+// const ReactMde = R.default
 import Showdown from "showdown"
 
 export default function Editor({ currentNote, updateNote }) {
