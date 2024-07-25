6ff6f49eabf6de56dcd92ebddd3acb0768efdcc1
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-07-25 14:34:37 +0200

    add refactor for everything

--- components/Editor.jsx
+++ components/Editor.jsx
@@ -4,28 +4,58 @@ import ReactMde from "react-mde"
 import Showdown from "showdown"
 
 export default function Editor({ currentNote, updateNote }) {
-    const [selectedTab, setSelectedTab] = React.useState("write")
+/*
 
-    const converter = new Showdown.Converter({
-        tables: true,
-        simplifiedAutoLink: true,
-        strikethrough: true,
-        tasklists: true,
-    })  
 
-    return (
-        <section className="pane editor">
-            <ReactMde
-                value={currentNote.body}
-                onChange={updateNote}
-                selectedTab={selectedTab}
-                onTabChange={setSelectedTab}
-                generateMarkdownPreview={(markdown) =>
-                    Promise.resolve(converter.makeHtml(markdown))
-                }
-                minEditorHeight={80}
-                heightUnits="vh"
-            />
-        </section>
-    )
+
+const [selectedTab, setSelectedTab] = React.useState("write")
+
+const converter = new Showdown.Converter({
+    tables: true,
+    simplifiedAutoLink: true,
+    strikethrough: true,
+    tasklists: true,
+})  
+
+return (
+    <section className="pane editor">
+        <ReactMde
+            value={tempNoteText}
+            onChange={setTempNoteText}
+            selectedTab={selectedTab}
+            onTabChange={setSelectedTab}
+            generateMarkdownPreview={(markdown) =>
+                Promise.resolve(converter.makeHtml(markdown))
+            }
+            minEditorHeight={80}
+            heightUnits="vh"
+        />
+    </section>
+)
+*/
+
+const [selectedTab, setSelectedTab] = React.useState("write")
+
+const converter = new Showdown.Converter({
+    tables: true,
+    simplifiedAutoLink: true,
+    strikethrough: true,
+    tasklists: true,
+})  
+
+return (
+    <section className="pane editor">
+        <ReactMde
+            value={currentNote.body}
+            onChange={updateNote}
+            selectedTab={selectedTab}
+            onTabChange={setSelectedTab}
+            generateMarkdownPreview={(markdown) =>
+                Promise.resolve(converter.makeHtml(markdown))
+            }
+            minEditorHeight={80}
+            heightUnits="vh"
+        />
+    </section>
+)
 }
